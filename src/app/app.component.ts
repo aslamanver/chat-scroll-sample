19e6721dda459e272b0455bc8d710802d23efaa8
Aslam Anver <slam.anver9@gmail.com>
2019-12-21 01:49:23 +0530

    Added contents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'chat-scroll-sample';
+export class AppComponent implements OnInit {
+
+  @ViewChild('chatscroll', { static: false }) chatscroll: ElementRef;
+
+  chats = [];
+  isLoading = false;
+
+  ngOnInit() {
+    this.addChats(50).then(() => {
+      setTimeout(() => {
+        // Scroll to bottom first time
+        this.chatscroll.nativeElement.scrollTop = this.chatscroll.nativeElement.scrollHeight;
+      }, 1);
+    });
+  }
+
+  addChats(c: number) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const oLength = this.chats.length;
+        for (let k = oLength; k < oLength + c; k++) {
+          this.chats.unshift(k + ' World');
+        }
+        resolve('Loaded');
+      }, 1000);
+    });
+  }
+
+  onScroll($event) {
+
+    const elem: HTMLElement = $event.srcElement;
+    const oScrollTop = elem.scrollTop;
+
+    if (this.isLoading) { return; }
+
+    if (elem.scrollTop < 50) {
+      this.isLoading = true;
+      this.addChats(50).then(() => {
+        this.isLoading = false;
+        elem.scrollTop = oScrollTop;
+      });
+    }
+  }
 }
